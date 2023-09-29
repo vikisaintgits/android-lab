54445d6e0d64c279cbe7ece8dba07258f41a300e
ANDROID LAB <ANDROID@saintgits.org>
2023-09-29 11:51:34 +0530

    listview app

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.android_lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String arr[]={"Akash","Vishnu","Unni","Sreelakshmi"};
+    ListView l1;
+    ArrayAdapter <String> arrAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arrAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,arr);
+        l1=findViewById(R.id.listview1);
+        l1.setAdapter(arrAdapter);
+        l1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String x=(String) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, x, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
