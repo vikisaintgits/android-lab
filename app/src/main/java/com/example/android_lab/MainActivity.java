bea50ea24928f6e89622d26c52cd3d872b426bb6
ANDROID LAB <ANDROID@saintgits.org>
2023-10-06 12:18:55 +0530

    added menu listener

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.android_lab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
     getMenuInflater().inflate(R.menu.main_menu, menu);
     return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.t1){
+            Toast.makeText(this, "first_item", Toast.LENGTH_SHORT).show();
+
+        }
+        if(item.getItemId()==R.id.t2){
+            Toast.makeText(this, "Second_item", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
